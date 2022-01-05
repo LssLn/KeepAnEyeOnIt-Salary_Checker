6f298798c931dac62e7d89d92fec29fab8d7bed9
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-01-05 17:33:44 +0100

    Add files via upload

--- Class/FileHandler.java
+++ Class/FileHandler.java
@@ -1,10 +1,17 @@
 package SalaryChecker.Class;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Set;
 
 /* 
  * 	It gives persistence to the program by:
@@ -24,7 +31,7 @@ import java.util.HashMap;
 public class FileHandler {
 	
 	
-	private String myFile="...";
+	private String myFile="C:\\Users\\RedBite\\Documents\\workspace-spring-tool-suite-4-4.12.0.RELEASE\\Esercizio 26 SalaryChecker\\src\\SalaryChecker\\write.txt";
 
 	
 	public String getMyFile() {
@@ -43,24 +50,139 @@ public class FileHandler {
             BufferedWriter bw=new BufferedWriter(fw);  
             for (Salary s : salary_YY.values()) {
     		    // reads all the Salaries in the hashmap
-            	bw.write(s.getMonth()+ " | " +s.getIncome()+" | "+s.getTotal_outcome()+"\n"); //when reading, set up the regex for |
+            	bw.write(s.getMonth()+ " " +s.getIncome()+" "+s.getTotal_outcome()+"\n"); //when reading, set up the regex for |
             	//to write the outcomes too, i added a new method in Salary: getHashMap (returns the HashMap)
+            	
             	HashMap<Integer, Outcome> outcomes = s.getOutcomes();
+            	int count=0;
             	for(Outcome o : outcomes.values()){
-            		bw.write(o.getOutcome()+" | "+o.getDescription()+"\n");
+            		count++;
+            	}
+            	bw.write(count+"\n");
+                bw.flush();
+                
+            	for(Outcome o : outcomes.values()){
+            		bw.write(o.getOutcome()+" "+o.getDescription()+"\n");
                     bw.flush();
             	}
     		}
         }catch(IOException e) {
+        	//throw new FileNotFoundException("The file described is nowhere to be found.");
         	System.out.println("\nFile non trovato: \n"+e.getStackTrace());
         }
-		
 	}
 	
 	
 	/*
-	public void readingFile() {
+	 * Given a txt file, the method fills the HashMap with the read values and returns it.
+	 */
+	public HashMap<Integer,Salary> readingFile() {
+		HashMap<Integer,Salary> Salary_2021 = new HashMap<>(12);
+		int ret_value=0;
+		System.out.println("before try");
+		try {
+			File file = new File(myFile);
+			Scanner scanner = new Scanner(file);
+			String fileContent = "";
+			System.out.println("before while");
+			while(scanner.hasNext()){ //reads the whole file
+				System.out.println("while is running");
+				try {
+					String month = scanner.next(); 
+					System.out.println("month::"+month);
+					month=month.toUpperCase();
+					ret_value=MonthToInt(month);
+					
+					Double income=Double.parseDouble(scanner.next());
+					System.out.println(" - - "+income);
+					Double outcome=Double.parseDouble(scanner.next());
+					System.out.println(" - - "+outcome);
+					
+					if(ret_value != -1) {
+			 			Salary salary = new Salary(income,month);
+			 			Salary_2021.put(ret_value-1, salary); 
+			 			//ret_value - 1 because index start from 0 up to 11, while months from 1 to 12
+			 			//it's critical to subtract -1 from ret value now because it may broke the do while
+			 			
+			 			Integer n_s_outcomes = Integer.parseInt(scanner.next());
+						System.out.println(" - - "+n_s_outcomes);
+	                    for(int i=0;i<n_s_outcomes;i++) {
+	                    	System.out.println("-:: sub element "+i+1);
+	                    	try {
+	                    		Double s_outcome = Double.parseDouble(scanner.next());
+		                    	System.out.println("-:: :: expense "+s_outcome);
+		                    	
+		                        String description = scanner.nextLine();
+		                        System.out.println(" - "+description);
+
+		                        salary.setSingle_outcome(s_outcome, description);
+
+	                    	} catch (NumberFormatException e) {
+	                    	    e.printStackTrace();
+	                    	}
+		                    	
+	                    }
+					}
+				}catch(InputMismatchException ex){
+                  //scanner.nextLine(); //skips the line if there is an error.
+					ex.printStackTrace();
+				}
+//				catch(NoSuchElementException ex) {
+//					System.out.println("launch \"NoSuchElementException\" "
+//							+ "customized following the fix provided by hasNext instead of hasNextLine"
+//							+ "https://stackoverflow.com/a/58853018/8422732");
+//				}
+			}	
+			//reading file while ended
+            return Salary_2021;
+
+		}catch (Exception e) {
+            e.printStackTrace();
+        }
+//        finally {
+//            // Always close the BufferedReader
+//            if (br != null) {
+//                try {
+//                    br.close();
+//                }
+//                catch (Exception e) {
+//                };
+//            }
+//        }
 		
+		return Salary_2021;
 	}
-	*/
+	
+	/* Used in readingFile, to convert month String to integer*/
+	public static Integer MonthToInt(String month) {
+		switch(month) {
+			case "JANUARY":
+				return 1;
+			case "FEBRUARY":
+					return 2;
+			case "MARCH":
+					return 3;
+			case "APRIL":
+					return 4;
+			case "MAY":
+					return 5;
+			case "JUNE":
+					return 6;
+			case "JULY":
+					return 7;
+			case "AUGUST":
+					return 8;
+			case "SEPTEMBER":
+					return 9;
+			case "OCTOBER":
+					return 10;
+			case "NOVEMBER":
+					return 11;
+			case "DECEMBER":
+					return 12;
+			default:
+					System.out.println("!!! This month is not an option. The format is (January, February, March, ...)\n");
+					return -1;
+		}
+	}
 }
