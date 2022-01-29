8b4d1cdf56d15efeaaae50b7f7807812bd78b1d1
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-01-29 10:49:02 +0100

    Update Year.java

--- Class/Year.java
+++ Class/Year.java
@@ -1,1 +1,35 @@
+package SalaryChecker.Class;
 
+import java.util.HashMap;
+
+public class Year implements Comparable<Year>{
+	private String year;
+	private HashMap<Integer,Salary> months = new HashMap<>(12);
+	
+	public Year(String year) {
+		this.year = year;
+	}
+	
+	public String getYear() {
+		return year;
+	}
+	
+	public HashMap<Integer,Salary> getMonths(){
+		return months;
+	}
+	
+	public void addMonth(Integer index, Salary salary) {
+		months.put(index,salary);
+		
+	}
+	
+	/*
+	 * Removes a Salary from the HashMap, given its corrispective month Index.
+	 * Returns the Salary (removal successfull) or null.
+	 */
+	public Salary removeMonth(Integer index) {
+		return months.remove(index);
+	}
+
+	
+}
