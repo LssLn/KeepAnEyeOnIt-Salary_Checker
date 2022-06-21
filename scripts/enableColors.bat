b51dd2a12869b4131449a86e0fe82fe4b6c38fcb
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-06-21 22:19:47 +0200

    Update enableColors.bat

--- scripts/enableColors.bat
+++ scripts/enableColors.bat
@@ -1,5 +1,5 @@
-# ** PROVIDED BY https://www.codeproject.com/Tips/5255355/How-to-Put-Color-on-Windows-Console ** 
-# to add colors to cmd
+rem ** PROVIDED BY https://www.codeproject.com/Tips/5255355/How-to-Put-Color-on-Windows-Console ** 
+rem to add colors to cmd
 reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000001 /f
-# to undo add colors to cmd
+rem to undo add colors to cmd
 reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000000 /f
