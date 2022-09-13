98a7efbebbeee04a8a81685bb14662ae5304439d
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-09-13 09:44:05 +0200

    Update and rename src/SalaryChecker/scripts/enableColors.bat to scripts/enableColors.bat

--- scripts/enableColors.bat
+++ scripts/enableColors.bat
@@ -2,4 +2,4 @@
 :: to add colors to cmd
 reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000001 /f
 :: to undo add colors to cmd
-reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000000 /f
+:: reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000000 /f
